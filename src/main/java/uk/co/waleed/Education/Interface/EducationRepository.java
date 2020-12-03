2c7c15e8d54c3528f49f24112cc1d0a186d40d86
Waleed Ur Rehman <waleedrehman@yahoo.co.uk>
2020-12-03 15:41:25 +0000

    changes to have the data being returned from the database

--- src/main/java/uk/co/waleed/Education/Interface/EducationRepository.java
+++ src/main/java/uk/co/waleed/Education/Interface/EducationRepository.java
@@ -1,10 +1,9 @@
 package uk.co.waleed.Education.Interface;
 
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
 import uk.co.waleed.Education.Model.EducationDTO;
 
 public interface EducationRepository extends CrudRepository<EducationDTO, Integer> {
-
 
 }
